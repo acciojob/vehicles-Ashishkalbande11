0cc504ae324d2c012669b1ad0389c99abd9e1a2b
Ashishkalbande11 <ashishkalbande6@gmail.com>
2024-03-16 14:23:47 +0530

    second Attempt

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -34,7 +34,34 @@ public class Car extends Vehicle {
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-
+        super.move(newSpeed,newDirection);;
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
+    }
+    public void steer(int direction){
+        super.steer(direction);
+    }
+    public void stop(){
+        super.stop();
+    }
+    public int getWheels(){
+       return wheels;
+    }
+    public String getType(){
+        return type;
+    }
+    public int getDoors(){
+        return doors;
+    }
+    public int getGears(){
+        return gears;
+    }
+    public boolean isManual(){
+        return isManual;
+    }
+    public int getSeats(){
+        return seats;
+    }
+    public int getCurrentGear(){
+        return currentGear;
     }
 }
